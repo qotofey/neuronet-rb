9db5e1f6f2edf1cc061ef10f5cc7e0fecaf571e4
Timofey Shufletyuk <qotofey@gmail.com>
2021-08-01 00:23:08 +0800

    ruby version to 3.0.2

--- lib/neuronet/layer.rb
+++ lib/neuronet/layer.rb
@@ -1,2 +1,15 @@
+require 'matrix'
+
 class Neuronet::Layer
+  attr_accessor :weights
+
+  def initialize(inputs_count, outputs_count)
+    @weights = generate_weights_matrix(inputs_count, outputs_count)
+  end
+
+  private
+
+  def generate_weights_matrix(inputs_count, outputs_count)
+    Matrix.build(inputs_count, outputs_count) { rand(-1.0..1.0) }
+  end
 end
