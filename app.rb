89b414f36c519c3dcf85fdd5f1973c6c206a34dc
Timofey Shufletyuk <qotofey@gmail.com>
2021-10-01 23:02:21 +0800

    set start with docker

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@
 
 require_relative 'lib/neuronet/net'
 
-net = Neuronet::Net.new(3, 2)
-net.learn([], 10_000, 0.3, 0.0125)
-puts Neuronet::Net.new(3, 2).inputs_count
+# net = Neuronet::Net.new(3, 2)
+# net.learn([], 10_000, 0.3, 0.0125)
+# puts Neuronet::Net.new(3, 2).inputs_count
+
+puts 'Hello form Docker!'
