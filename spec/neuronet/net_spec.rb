901b76b9fa755af3f7f138d848c337c0645458f2
Timofei Shufletiuk <qotofey@gmail.com>
2022-04-10 18:52:41 +0800

    fix

---
+++ spec/neuronet/net_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Neuronet::Layer do
+  let(:activation_function) { :void }
+
+  describe '#predict' do
+
+  end
+
+  describe '#learn' do
+
+  end
+end
