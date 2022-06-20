ed09d5c404fc226c5861592d0e26785b383d8e6c
Timofei Shufletiuk <qotofey@gmail.com>
2022-06-20 23:26:46 +0800

    fix app

--- app.rb
+++ app.rb
@@ -1,25 +1,36 @@
 # frozen_string_literal: true
 
-require 'ruby-progressbar'
+# require 'ruby-progressbar'
 require_relative 'lib/neuronet/net'
 require_relative 'lib/neuronet/configuration'
 
-net = Neuronet::Net.new(3, 2)
-net.learn([], 10_000, 0.3, 0.0125)
-puts Neuronet::Net.new(3, 2).inputs_count
+info = <<~startdoc
+  Добро пожаловать в демонстрационный интерфейс модуля ИНС!
+
+  Для прекращения работы программы введите `0` или `exit` (без символа `)\n
+startdoc
+
+puts info
+
+# net = Neuronet::Net.new(3, 2)
+# net.learn([], 10_000, 0.3, 0.0125)
+# puts Neuronet::Net.new(3, 2).inputs_count
 
 
-PARTS_OF_WORK = 100
+# PARTS_OF_WORK = 100
 
-learning_progressbar = ProgressBar.create(
-  total: PARTS_OF_WORK,
-  format: '%a, %J, %E %B' # elapsed time, percent complete, estimate, bar
-  # output: File.open(File::NULL, 'w') # IN TEST ENV
-)
+# learning_progressbar = ProgressBar.create(
+#   total: PARTS_OF_WORK,
+#   format: '%a, %J, %E %B' # elapsed time, percent complete, estimate, bar
+#   # output: File.open(File::NULL, 'w') # IN TEST ENV
+# )
 
-(1..PARTS_OF_WORK).each do |_i|
-  sleep(1)
-  learning_progressbar.increment
-end
+# (1..PARTS_OF_WORK).each do |_i|
+#   sleep(1)
+#   learning_progressbar.increment
+# end
 
-puts 'Hello from Docker!'
+puts DATA.read
+
+__END__
+Hello from Docker!
